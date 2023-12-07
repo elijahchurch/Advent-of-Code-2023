deaa83fcbad168e64ac205c2a7c179940d6e5a74
Jason E. Church <elijahchurch@gmail.com>
2023-12-07 12:42:05 -0800

    Set up files and structure input data

---
+++ src/_tests_/CubeProbability.test.tsx
@@ -0,0 +1,7 @@
+import { setAnalyzer } from "../AdventFunctions/CubeProbability"
+
+describe ('CubeProbability', () => {
+    test('Sequence Analyzer returns a boolean', () => {
+        const result = setAnalyzer(["1R", "20B", "30G"])
+    })
+})
