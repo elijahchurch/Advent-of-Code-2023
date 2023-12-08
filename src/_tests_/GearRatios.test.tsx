72f0f5f3a11cb1903ced36135d1b3195a07e3429
Jason E. Church <elijahchurch@gmail.com>
2023-12-08 10:16:07 -0800

    Pass readFileIntoStringArray test

--- src/_tests_/GearRatios.test.tsx
+++ src/_tests_/GearRatios.test.tsx
@@ -3,8 +3,7 @@ import { readFileIntoStringArray } from "../readFile";
 describe("readFileIntoStringArray",  () => {
     
     test("readFileIntoStringArray should take raw data and turn it into a string array", async () => {
-        const result = await readFileIntoStringArray("Advent-of-Code-2023/src/data/GearRatiosData");
-        console.log(result);
+        const result = await readFileIntoStringArray("src/data/GearRatiosData");
         const line = ".........398.............551.....................452..................712.996.................646.40...1.....875..958.553...................";
         expect(result).toContainEqual(line);
     });
