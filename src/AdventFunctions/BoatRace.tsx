f61660cc57889ec449fb52ed7031359e06e629f8
Jason E. Church <elijahchurch@gmail.com>
2023-12-12 14:38:48 -0800

    Pass test for winningRecordOptions;

--- src/AdventFunctions/BoatRace.tsx
+++ src/AdventFunctions/BoatRace.tsx
@@ -1,6 +1,12 @@
 import { IRace } from "../data/BoatRaceData";
 
 export const winningRecordOptions = (race: IRace) : number => {
-    let result = 0;
-    return result;
+    let winningOptions = 0;
+    for (let i = 0; i <= race.time; i++) {
+        const boatDistance = i*(race.time - i);
+        if(boatDistance > race.distance) {
+            winningOptions +=1
+        }
+    }
+    return winningOptions;
 }
