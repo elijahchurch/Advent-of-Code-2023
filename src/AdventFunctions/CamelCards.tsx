815fd4161c4782cfe77194f481d9d0f7d8e63ff3
Jason E. Church <elijahchurch@gmail.com>
2023-12-13 13:28:08 -0800

    Pass initial test for determineStrongerHand

--- src/AdventFunctions/CamelCards.tsx
+++ src/AdventFunctions/CamelCards.tsx
@@ -1,4 +1,4 @@
-export const handStrength = {
+export const typeStrength = {
     highCard: 1,
     onePair: 2,
     twoPair: 3,
@@ -6,6 +6,22 @@ export const handStrength = {
     fullHouse: 5,
     fourOfAKind: 6,
     fiveOfAKind: 7,
+}
+
+export const handStrength = {
+    "2": 1,
+    "3": 2,
+    "4": 3,
+    "5": 4,
+    "6": 5,
+    "7": 6,
+    "8": 7,
+    "9": 8,
+    "T": 9,
+    "J": 10,
+    "Q": 11,
+    "K": 12,
+    "A": 13,
 }
 
 export const determineHandType = (hand: string[]) : string => {
@@ -38,5 +54,15 @@ export const determineHandType = (hand: string[]) : string => {
         else {
             return "highCard";
         }
+    }
+}
+
+export const determineStrongerHand = (index: number, hand1 : string[], hand2: string[]) : number => {
+    if(handStrength[(hand1[index])] > handStrength[(hand2[index])]) {
+        return 1;
+    } else if(handStrength[(hand1[index])] < handStrength[hand2[index]]) {
+        return -1;
+    } else {
+        return determineStrongerHand(index +1, hand1, hand2);
     }
 }
--- src/_tests_/CamelCards.test.tsx
+++ src/_tests_/CamelCards.test.tsx
@@ -1,46 +1,53 @@
-import { determineHandType } from "../AdventFunctions/CamelCards";
+import { determineHandType, determineStrongerHand } from "../AdventFunctions/CamelCards";
 
 describe("CamelCards and helper functions", () => {
 
-    test("determineCardWorth can determine fiveOfAKind", () => {
+    test("determineHandType can determine fiveOfAKind", () => {
         const hand = ["a", "a", "a", "a", "a"];
         const result = determineHandType(hand);
         expect(result).toEqual("fiveOfAKind");
     })
 
-    test("determineCardWorth can determine fourOfAKind", () => {
+    test("determineHandType can determine fourOfAKind", () => {
         const hand = ["a", "1", "a", "a", "a"];
         const result = determineHandType(hand);
         expect(result).toEqual("fourOfAKind");
     })
 
-    test("determineCardWorth can determine fullHouse", () => {
+    test("determineHandType can determine fullHouse", () => {
         const hand = ["1", "a", "1", "a", "1"];
         const result = determineHandType(hand);
         expect(result).toEqual("fullHouse");
     })
 
-    test("determineCardWorth can determine threeofAKind", () => {
+    test("determineHandType can determine threeofAKind", () => {
         const hand = ["1", "a", "x", "a", "a"];
         const result = determineHandType(hand);
         expect(result).toEqual("threeOfAKind");
     })
 
-    test("determineCardWorth can determine twoPair", () => {
+    test("determineHandType can determine twoPair", () => {
         const hand = ["1", "a", "x", "a", "x"];
         const result = determineHandType(hand);
         expect(result).toEqual("twoPair");
     })
 
-    test("determineCardWorth can determine onePair", () => {
+    test("determineHandType can determine onePair", () => {
         const hand = ["1", "d", "x", "c", "x"];
         const result = determineHandType(hand);
         expect(result).toEqual("onePair");
     })
 
-    test("determineCardWorth can determine highCard", () => {
+    test("determineHandType can determine highCard", () => {
         const hand = ["1", "d", "x", "c", "V"];
         const result = determineHandType(hand);
         expect(result).toEqual("highCard");
+    })
+
+    test("determineStrongerHand returns 1 if the first hand's first card is higher", () => {
+        const hand1 = ["A", "2", "T", "3", "7"];
+        const hand2 = ["J", "3", "4", "5", "6"];
+        const result = determineStrongerHand(0, hand1, hand2);
+        expect(result).toEqual(1)
     })
 })
